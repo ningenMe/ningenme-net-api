eb331e778e190870fa81d6fc46f21456aabeab86
ningenMe <amohkuru@gmail.com>
2020-08-10 11:53:35 +0900

    [modify] deploy

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ do
 value=`sudo -u ec2-user aws ssm get-parameters --name $key --query "Parameters[*].{Value: Value}" --output text`
 export $key=$value
 done
-pid=`jps | awk -F: '/ningenme.net.api.jar/{print $1}' | awk '{print $1}'`
+pid=`jps | awk -F: '/api-0.0.1-SNAPSHOT/{print $1}' | awk '{print $1}'`
 if [ $pid != "" ] ; then
     kill -9 $pid
 fi
-java -jar build/libs/ningenme.net.api.jar &
+java -jar build/libs/api-0.0.1-SNAPSHOT.jar &
 curl -X POST --data-urlencode "payload={\"text\":\"succeeded api-java deploy\",\"channel\":\"#log-info\",}" $SLACK_WEBHOOK_URL
