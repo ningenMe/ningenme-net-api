7c882724e41b8c0dd6044146ef9fcbebbc366070
ningenMe <amohkuru@gmail.com>
2020-11-04 00:26:36 +0900

    [modify] テスト

--- src/test/groovy/ningenme/net/api/ApiApplicationSpec.groovy
+++ src/test/groovy/ningenme/net/api/ApiApplicationSpec.groovy
@@ -1,10 +1,7 @@
 package ningenme.net.api
 
-import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification;
 
-
-@SpringBootTest
 public class ApiApplicationSpec extends Specification{
 	def sampleTest() {
 		expect:
