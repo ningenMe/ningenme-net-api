c359c05ac7c20b4d9abde1904014a74998cc8d6b
ningenMe <amohkuru@gmail.com>
2020-11-22 06:07:31 +0900

    [add] cron

---
+++ cron/update_atcoder_id.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+res=`curl -X POST -d "email=${email}" -d "password=${password}" --dump-header - https://api.ningenme.net/v1/login | grep x-amzn-remapped-authorization:`
+header=`echo $res | awk -F : '{print $2}'`
+
+header="\"authorization: "${header}\"
+header=`echo $header | sed "s/\r//g"`
+host="https://api.ningenme.net/v1/compro/sites/AtCoder/users"
+curl="curl -X PUT -H ${header} $host"
+eval ${curl}
