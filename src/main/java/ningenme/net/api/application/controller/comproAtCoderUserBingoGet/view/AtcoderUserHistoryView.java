31aa1b281350b9601e94a99f9c672eb92d9242c4
ningenMe <amohkuru@gmail.com>
2020-12-31 04:37:02 +0900

    [add] url

--- src/main/java/ningenme/net/api/application/controller/comproAtCoderUserBingoGet/view/AtcoderUserHistoryView.java
+++ src/main/java/ningenme/net/api/application/controller/comproAtCoderUserBingoGet/view/AtcoderUserHistoryView.java
@@ -10,12 +10,14 @@ import ningenme.net.api.domain.entity.AtcoderUserHistory;
 public class AtcoderUserHistoryView {
   private final String atcoderId;
   private final String contestId;
+  private final String url;
   private final Integer place;
 
   public static AtcoderUserHistoryView of(AtcoderUserHistory atcoderUserHistory) {
     return new AtcoderUserHistoryView(
             atcoderUserHistory.getAtcoderId().getValue(),
             atcoderUserHistory.getContestId().getValue(),
+            atcoderUserHistory.getUrl().getValue(),
             atcoderUserHistory.getPlace().getValue()
     );
   }
