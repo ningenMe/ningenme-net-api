2f4406d6e7ea83cabf6c9364b16d66793842fdf0
ningenMe <amohkuru@gmail.com>
2020-12-31 03:41:37 +0900

    [modify] view

--- src/main/java/ningenme/net/api/application/controller/comproCategoryGenreListWithTopicGet/ComproCategoryGenreListWithTopicGetController.java
+++ src/main/java/ningenme/net/api/application/controller/comproCategoryGenreListWithTopicGet/ComproCategoryGenreListWithTopicGetController.java
@@ -1,31 +1,32 @@
-package ningenme.net.api.application.controller.comproCategoryGenreListGet;
+package ningenme.net.api.application.controller.comproCategoryGenreListWithTopicGet;
 
 import io.swagger.annotations.*;
-import lombok.RequiredArgsConstructor;
 import ningenme.net.api.domain.service.ComproCategoryGenreService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @Api(tags = {"compro-category"})
 @RestController
 @RequiredArgsConstructor
-public class ComproCategoryGenreListGetController {
+public class ComproCategoryGenreListWithTopicGetController {
   private final ComproCategoryGenreService comproCategoryGenreService;
 
-  @ApiOperation(value = "ComproCategoryGenre 複数参照API")
+  @ApiOperation(value = "ComproCategoryGenreWithTopic 複数参照API")
   @ApiResponses(value = {
           @ApiResponse(code = 200, message = "OK"),
           @ApiResponse(code = 400, message = "Bad Request"),
           @ApiResponse(code = 500, message = "Internal Server Error")
   })
-  @GetMapping("/v1/compro/category/genres")
-  public ResponseEntity<ComproCategoryGenreListGetResponse> get() {
+  @GetMapping("/v1/compro/category/genres/topics")
+  public ResponseEntity<ComproCategoryGenreListWithTopicGetResponse> get() {
     return ResponseEntity
             .ok()
             .body(
-                    ComproCategoryGenreListGetResponse.of(
-                            comproCategoryGenreService.get()
+                    ComproCategoryGenreListWithTopicGetResponse.of(
+                            comproCategoryGenreService.getWithTopic()
                     )
             );
   }
